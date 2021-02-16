c5ef8073c177be7c8d2f76b13314c3e264d4d7e6
乌龟来袭 <43867943+WuGuiLaiXi@users.noreply.github.com>
2021-02-16 09:31:57 +0800

    Create day20210216.java

---
+++ everyday_question/day20210216.java
@@ -0,0 +1,114 @@
+package com.leetcode.every_question;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class day20210216 {
+/*
+ * 561. 数组拆分 I
+给定长度为 2n 的整数数组 nums ，你的任务是将这些数分成 n 对, 例如 (a1, b1), (a2, b2), ..., (an, bn) ，使得从 1 到 n 的 min(ai, bi) 总和最大。
+
+返回该 最大总和 。
+
+ 
+
+示例 1：
+
+输入：nums = [1,4,3,2]
+输出：4
+解释：所有可能的分法（忽略元素顺序）为：
+1. (1, 4), (2, 3) -> min(1, 4) + min(2, 3) = 1 + 2 = 3
+2. (1, 3), (2, 4) -> min(1, 3) + min(2, 4) = 1 + 2 = 3
+3. (1, 2), (3, 4) -> min(1, 2) + min(3, 4) = 1 + 3 = 4
+所以最大总和为 4
+示例 2：
+
+输入：nums = [6,2,6,5,1,2]
+输出：9
+解释：最优的分法为 (2, 1), (2, 5), (6, 6). min(2, 1) + min(2, 5) + min(6, 6) = 1 + 2 + 6 = 9
+ 
+
+提示：
+
+1 <= n <= 104
+nums.length == 2 * n
+-104 <= nums[i] <= 104
+ * */
+	
+    public int arrayPairSum(int[] nums) {
+    	int mun = 0;
+    	int[] arr = nums;
+        int i = 0;
+    	//HeapSort sort = new HeapSort();
+    	//sort.heapSort(arr);
+    	Arrays.sort(nums);
+        while (i < nums.length) {
+            mun += nums[i];
+            i += 2;
+        }
+    	return mun;
+    }
+}
+//堆排序
+class HeapSort {
+	int[] arr = new int[20000];
+
+    /**
+     * 创建堆，
+     * @param arr 待排序列
+     */
+    public void heapSort(int[] arr) {
+        //创建堆
+        for (int i = (arr.length - 1) / 2; i >= 0; i--) {
+            //从第一个非叶子结点从下至上，从右至左调整结构
+            adjustHeap(arr, i, arr.length);
+        }
+
+        //调整堆结构+交换堆顶元素与末尾元素
+        for (int i = arr.length - 1; i > 0; i--) {
+            //将堆顶元素与末尾元素进行交换
+            int temp = arr[i];
+            arr[i] = arr[0];
+            arr[0] = temp;
+
+            //重新对堆进行调整
+            adjustHeap(arr, 0, i);
+        }
+    }
+
+    /**
+     * 调整堆
+     * @param arr 待排序列
+     * @param parent 父节点
+     * @param length 待排序列尾元素索引
+     */
+    public void adjustHeap(int[] arr, int parent, int length) {
+        //将temp作为父节点
+        int temp = arr[parent];
+        //左孩子
+        int lChild = 2 * parent + 1;
+
+        while (lChild < length) {
+            //右孩子
+            int rChild = lChild + 1;
+            // 如果有右孩子结点，并且右孩子结点的值大于左孩子结点，则选取右孩子结点
+            if (rChild < length && arr[lChild] < arr[rChild]) {
+                lChild++;
+            }
+
+            // 如果父结点的值已经大于孩子结点的值，则直接结束
+            if (temp >= arr[lChild]) {
+                break;
+            }
+
+            // 把孩子结点的值赋给父结点
+            arr[parent] = arr[lChild];
+
+            //选取孩子结点的左孩子结点,继续向下筛选
+            parent = lChild;
+            lChild = 2 * lChild + 1;
+        }
+        arr[parent] = temp;
+    }
+}
